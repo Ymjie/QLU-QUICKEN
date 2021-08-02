db1622db966e4432cb243602b6029b0f8ebbd3d9
Vshex <201904300348@stu.qlu.edu.cn>
2021-08-02 23:47:27 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,18 @@
+window.settings = {
+  // 站点标题
+  title: 'QLU-QUICKEN',
+  // 站点描述
+  description: '纯公益机场',
+  // API
+  host: 'http://www.vshex.com',
+  // 主题
+  theme: {
+    sidebar: 'dark',
+    header: 'dark',
+    color: 'black'
+  },
+  // 背景
+  background_url: 'https://z3.ax1x.com/2021/05/07/g3z8MD.jpg',
+  // crisp
+  crisp_id: ''
+}
