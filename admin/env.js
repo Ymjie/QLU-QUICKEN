b0e65a8786592935332d51d87a9cd31a6e2c77b5
Vshex <201904300348@stu.qlu.edu.cn>
2021-08-04 20:37:50 +0800

    Add files via upload

---
+++ admin/env.js
@@ -0,0 +1,14 @@
+window.settings = {
+  // 站点标题
+  title: 'QLU-QUICKEN',
+  // API
+  host: 'https://www.vshex.com',
+  // 主题
+  theme: {
+    sidebar: 'dark',
+    header: 'dark',
+    color: 'black'
+  },
+  // 背景
+  background_url: 'https://z3.ax1x.com/2021/05/07/g3z8MD.jpg'
+}
